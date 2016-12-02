e7347a312599089c68bcd7d3ecdb66a885c7a44b
fdsilva <neitonfsilva@gmail.com>
2016-12-02 00:37:22 -0300

    First app example

---
+++ SingleViewExample/SingleViewExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewExample
+//
+//  Created by MacBook Pro on 02/12/16.
+//  Copyright © 2016 MobilliaApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewExample/SingleViewExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SingleViewExample
+//
+//  Created by MacBook Pro on 02/12/16.
+//  Copyright © 2016 MobilliaApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
