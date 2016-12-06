057f0dee51ceeedfd098f8e8a1a0d0465151d4c5
fdsilva <neitonfsilva@gmail.com>
2016-12-06 13:57:55 -0300

    Getting start with swift data types

---
+++ gettingStartWithSwift.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var numero = 1 + 2
+
+//variaveis tipo string
+var user = "Xico"
+var nome: String = "Xico";
+//numero inteiros
+
+var inteiro = 10
+
+var numeroInteiro: Int = 5;
+
+numeroInteiro = numeroInteiro + inteiro;
+
+//Double
+var decimalNumber: Double
+
+decimalNumber = 9.4
+
+//Float
+var numeroFloat: Float
+
+numeroFloat = 3.1898
+
+//exibiçao
+print("Exibindo numero inteiro: " + String(numeroInteiro) + "Double: " + String(decimalNumber))
+
+//Chalenge_1
+var num1 : Int = 0;
+var num2 : Int = 0;
+
+num1 = 10;
+num2 = 20;
+
+var sum: Int = num1 + num2;
+
+print("A soma dos dois numeros é: " + String(sum));
+
+
+
+
+
+
